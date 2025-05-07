0b568ae364354aaeeae74e05ebc222126f121e79
AishuN1107 <naidusaiaishu2003@gmail.com>
2025-05-07 15:51:20 +1000

    changes

--- server.js
+++ server.js
@@ -1,47 +1,59 @@
 const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
 
-
 dotenv.config();
-const app = express();
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Attach io to app for use in routes
+app.set('io', io);
+
+// Middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("MongoDB Connection Error:", err));
 
-
+// Routes
 app.use('/auth', authRoutes);
 app.use('/appointments', appointmentRoutes);
 
-
+// Serve HTML files from views folder
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
-  });
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
 app.get('/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'register.html'));
-  });
-
+  res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});
 app.get('/dashboard.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
-  });
-
+  res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
+});
 app.get('/book.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'book.html'));
-  });
-const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+  res.sendFile(path.join(__dirname, 'views', 'book.html'));
+});
+app.get('/profile.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'profile.html'));
+});
 
+// Start server
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
